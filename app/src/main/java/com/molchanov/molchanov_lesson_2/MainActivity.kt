1c0b3734f4cd0d18a8a007f15d81c538c24aeada
lbuckl <vadimufastr@gmail.com>
2023-02-14 19:16:00 +0500

    Lesson_3_1_feature: Добавил binding в майн активити. Внимание!!! Приложение требует gradle jdk версии 11

--- app/src/main/java/com/molchanov/molchanov_lesson_2/MainActivity.kt
+++ app/src/main/java/com/molchanov/molchanov_lesson_2/MainActivity.kt
@@ -2,10 +2,15 @@ package com.molchanov.molchanov_lesson_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.molchanov.molchanov_lesson_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
