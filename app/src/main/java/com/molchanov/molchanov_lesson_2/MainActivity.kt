e6a3d848c512ef9fbf42b667032b2596ca94e36e
lbuckl <ya_mvn@mail.ru>
2023-02-15 18:46:03 +0500

    main: добавил биндинг в майн актвити

--- app/src/main/java/com/molchanov/molchanov_lesson_2/MainActivity.kt
+++ app/src/main/java/com/molchanov/molchanov_lesson_2/MainActivity.kt
@@ -2,10 +2,16 @@ package com.molchanov.molchanov_lesson_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.molchanov.molchanov_lesson_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
