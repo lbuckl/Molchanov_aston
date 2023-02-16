66ffa89d2e5ff2eb77aaae06aa96aa407ef2b72d
lbuckl <ya_mvn@mail.ru>
2023-02-16 17:30:03 +0500

    master: Добавил BaseActivity и MainFragment в будущем буду просто переименовывать и заполнять данными

---
+++ app/src/main/java/com/molchanov/molchanov_lesson_2/BaseActivity.kt
@@ -0,0 +1,33 @@
+package com.molchanov.molchanov_lesson_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.molchanov.molchanov_lesson_2.databinding.ActivityMainBinding
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+        setMainFragment(savedInstanceState)
+    }
+
+    /**
+     * Установка базового фрагмента в контейнер при запуске приложения
+     */
+    private fun setMainFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null)
+            addMainFragment()
+    }
+
+    /**
+     * Здесь необходимо реализовать добавление первого фрагмента в контейнер
+     */
+    abstract fun addMainFragment()
+}
--- app/src/main/java/com/molchanov/molchanov_lesson_2/MainActivity.kt
+++ app/src/main/java/com/molchanov/molchanov_lesson_2/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.molchanov.molchanov_lesson_2
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.molchanov.molchanov_lesson_2.databinding.ActivityMainBinding
+class MainActivity : BaseActivity() {
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        super.onCreate(savedInstanceState)
-
-        setContentView(binding.root)
+    override fun addMainFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(binding.container.id, MainFragment.instance, "flags_frame")
+            .commit()
     }
 }
